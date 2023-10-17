f63b5316bbf81641a88a6ef1b872201cacd302a3
kgv <kgv@users.noreply.github.com>
2023-10-17 20:44:08 +0300

    0.0.3

--- src/counter.rs
+++ src/counter.rs
@@ -51,7 +51,7 @@ impl Deref for Counter {
 impl Display for Counter {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         for (atom, count) in &self.0 {
-            write!(f, "{atom:?}")?;
+            f.write_str(atom.symbol())?;
             if count.get() > 1 {
                 write!(f, "{count}")?;
             }
@@ -98,13 +98,12 @@ impl FromStr for Counter {
 fn test() {
     use atom::isotopes::{C, H, O};
 
-    assert_eq!(
-        Ok(counter! {
-            C::Twelve => 2,
-            H::One => 5,
-            O::Sixteen => 1,
-            H::One => 1,
-        }),
-        "C2H5OH".parse()
-    );
+    let counter = counter! {
+        C::Twelve => 2,
+        H::One => 5,
+        O::Sixteen => 1,
+        H::One => 1,
+    };
+    assert_eq!(counter.to_string(), "H6C2O");
+    assert_eq!(Ok(counter), "C2H5OH".parse());
 }
